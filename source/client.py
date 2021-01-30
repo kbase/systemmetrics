385f6c317376601bf6b9cf4798f5fb3bff8a1e5c
Steve Chan <sychan168@gmail.com>
2021-01-29 16:28:31 -0800

    Enable testing of data collection by leaving out ES ingest endpoint

--- source/client.py
+++ source/client.py
@@ -1,15 +1,23 @@
 import socket
 import json
 import os
-host = os.environ["ELASTICSEARCH_HOST"]
+
+host = os.environ.get("ELASTICSEARCH_HOST")
+if host is None:
+    print("No value set for ELASTICSEARCH_HOST environment variable, output will be send to stdout")
+
+
 def to_logstashJson(log_d):
 
     port = 9000
     json_data = json.dumps(log_d)
     json_data += '\n'
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.sendall(json_data.encode())
+    if host is not None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.sendall(json_data.encode())
+    else:
+        print(json_data)
 
     return None
